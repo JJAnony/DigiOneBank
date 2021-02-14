d858ec2a60628e918053a5dd6262d959e8083d75
anonymous <jcolivati@gmail.com>
2021-02-14 16:40:09 -0300

    created Person class

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Main.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.digionebank
+
+import one.digitalinnovation.digionebank.model.Person
+
+fun main() {
+    val jj = Person();
+
+    println(jj.name)
+    println(jj.cpf)
+
+}
---
+++ src/main/kotlin/one/digitalinnovation/digionebank/model/Person.kt
@@ -0,0 +1,6 @@
+package one.digitalinnovation.digionebank.model
+
+class Person {
+    var name: String = "jj"
+    var cpf: String = "123.123.123.12"
+}

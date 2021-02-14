fe4c8c51a7343fb8421916f7bbbd1cb0366bbd47
anonymous <jcolivati@gmail.com>
2021-02-14 17:19:10 -0300

    created Account class

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/model/Account.kt
@@ -0,0 +1,15 @@
+package one.digitalinnovation.digionebank.model
+
+import java.math.BigDecimal
+
+class Account(val agency: String, val number: String, val balance: BigDecimal) {
+
+
+    fun deposit(value: BigDecimal) {
+
+    }
+
+    fun withdraw(value: BigDecimal) {
+
+    }
+}
